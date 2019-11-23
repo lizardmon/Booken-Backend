6e5e8d7a4489eb89e98dd00fd718342736d50615
Gil <sgy@tpay.co.kr>
2019-11-23 16:21:31 +0900

    [Config] Mash-up Config SecretManager 세팅 추가

--- sources/app/config/settings/production.py
+++ sources/app/config/settings/production.py
@@ -13,7 +13,7 @@ from .base import *  # noqa
 # ENVIRON
 # ------------------------------------------------------------------------------
 # https://github.com/LeeHanYeong/django-aws-secrets-manager
-AWS_SECRETS_MANAGER_SECRETS_SECTION = "<SECRETS_SECTION>"
+AWS_SECRETS_MANAGER_SECRETS_SECTION = "mash-up-book-worker:production"
 
 
 # GENERAL
