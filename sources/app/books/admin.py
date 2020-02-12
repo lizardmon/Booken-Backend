32bcb0da29dd92a1d7711053e43a5225d4895fef
Gil <sgy@tpay.co.kr>
2020-02-12 19:18:13 +0900

    책 리뷰 Admin 추가

--- sources/app/books/admin.py
+++ sources/app/books/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from books.models import Book
+from books.models import Book, BookReview
+
+
+class BookReviewInlineAdmin(admin.StackedInline):
+    model = BookReview
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    inlines = [BookReviewInlineAdmin]
